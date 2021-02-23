7e45a2f0e84bf99d49155a7ca2143b0b643f9dd1
Danil Vorozheykin <danilvda@gmail.com>
2021-02-23 21:24:41 +0300

    Add Practice 11

---
+++ practice11/src/test/java/ru/mirea/pr11/Practice11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.pr11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
