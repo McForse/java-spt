d4876e5b0cc3c02fed7396393f8c44949e897690
Danil Vorozheykin <danilvda@gmail.com>
2021-02-24 10:40:30 +0300

    Add Practice 13

---
+++ practice13/src/test/java/ru/mirea/pr13/Practice13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.pr13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
